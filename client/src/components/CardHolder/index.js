d470d8abbd2bd06cf73e611892f05df4abf549e8
lianadanae <lianabette@gmail.com>
2020-01-21 20:13:37 -0500

    added cardholder

---
+++ client/src/components/CardHolder/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function CardHolder() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+    // render() {
+    //     const {}= props
+    //     return (
+            // <div className="row">{
+            //     this.state.listings.map(listing=>{
+            //         return(
+            //             <div className="col-4">
+            //                 <div className="card">
+            //                     <div className="card-body">
+            //         <h5 className="card-title">{listing.address}</h5>
+            //                     </div>
+            //                 </div>
+            //             </div>
+            //         )
+            //     })
+            // }
+                
+            // </div>
+//         )
+//     }
+// }
+
+
+
+export default CardHolder
