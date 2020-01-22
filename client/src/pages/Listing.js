d0759d96e86bc5762fcdbee8920e1704fd1d94fb
lianadanae <lianabette@gmail.com>
2020-01-21 23:35:21 -0500

    added datepicker and cardHolder

--- client/src/pages/Listing.js
+++ client/src/pages/Listing.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
+//import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../api";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
+//import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
+import CardHolder from "../components/CardHolder";
+import { Card, Form, FormGroup } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+ 
+import "react-datepicker/dist/react-datepicker.css";
+//import SideNav from "../components/SideNav";
+// import { Chevron, Icon, NavLink, menuItems } from "../components/SideNav";
 
 class Listings extends Component {
   state = {
     listings: [],
     address: "",
-    dateListed: "",
-    status: ""
+    startDate: new Date(),
+    status: "",
+    notes: ""
   };
 
   componentDidMount() {
@@ -22,7 +30,7 @@ class Listings extends Component {
   loadListings = () => {
     API.getListings()
       .then(res =>
-        this.setState({ listings: res.data, address: "", dateListed: "", status: "" })
+        this.setState({ listings: res.data, address: "", startDate: new Date(), status: "", notes: "" })
       )
       .catch(err => console.log(err));
   };
@@ -40,13 +48,20 @@ class Listings extends Component {
     });
   };
 
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
   handleFormSubmit = event => {
     event.preventDefault();
-    if (this.state.address && this.state.dateListed) {
+    if (this.state.address && this.state.date) {
       API.saveListing({
         address: this.state.address,
-        dateListed: this.state.dateListed,
-        synopsis: this.state.synopsis
+        date: this.state.date,
+        status: this.state.status,
+        notes: this.state.notes
       })
         .then(res => this.loadListings())
         .catch(err => console.log(err));
@@ -61,38 +76,89 @@ class Listings extends Component {
             <Jumbotron>
               <h1>Add a new listing</h1>
             </Jumbotron>
-            <form>
+           <CardHolder></CardHolder>
+
+            <Form>
+              <FormGroup>
+              <Form.Label>Address</Form.Label>
               <Input
                 value={this.state.address}
                 onChange={this.handleInputChange}
                 name="address"
-                placeholder="Address (required)"
+                placeholder="(required)"
               />
-              <Input
-                value={this.state.dateListed}
-                onChange={this.handleInputChange}
-                name="dateListed"
+              <Form.Label>Date Listed</Form.Label>
+              <DatePicker
+                selected={this.state.date}
+                onSelect={this.handleSelect} //when day is clicked
+                onChange={this.handleChange} //only when value has changed
+                name="date"
                 placeholder="Date Listed (required)"
               />
-              <TextArea
+              <Form.Label>Status</Form.Label>
+              <Input
                 value={this.state.status}
                 onChange={this.handleInputChange}
                 name="status"
-                placeholder="Status (Optional)"
+                placeholder="(required)"
               />
+              <Form.Label>Notes</Form.Label>
+              <TextArea
+                value={this.state.notes}
+                onChange={this.handleInputChange}
+                name="notes"
+                placeholder="(Optional)"
+              />
+
               <FormBtn
-                disabled={!(this.state.dateListed && this.state.address)}
+                disabled={!(this.state.date && this.state.address)}
                 onClick={this.handleFormSubmit}
               >
                 Submit Listing
               </FormBtn>
-            </form>
+              </FormGroup>
+            </Form>
           </Col>
+
+          {/* Listings */}
           <Col size="md-6 sm-12">
             <Jumbotron>
               <h1>My Property Listings</h1>
             </Jumbotron>
-            {this.state.listings.length ? (
+            {this.state.listings.map(listing=>{
+            return(
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>{listing.address}</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  </Card.Body>
+  <Form>
+  <Form.Group controlId="formBasicCheckbox">
+  {['checkbox'].map(type => (
+    <div key={`custom-${type}`} className="mb-3">
+      <Form.Check 
+        custom
+        type={type}
+        id={`custom-${type}`}
+        label={`Check this custom ${type}`}
+      />
+      </div>
+      ))}
+      </Form.Group>
+      </Form>
+
+  <Card.Body>
+    <Card.Link href="#">Card Link</Card.Link>
+  </Card.Body>
+</Card>
+            )}
+        )}
+
+            {/* {this.state.listings.length ? (
               <List>
                 {this.state.listings.map(listing => (
                   <ListItem key={listing._id}>
@@ -107,7 +173,7 @@ class Listings extends Component {
               </List>
             ) : (
               <h3>No Results to Display</h3>
-            )}
+            )} */}
           </Col>
         </Row>
       </Container>
