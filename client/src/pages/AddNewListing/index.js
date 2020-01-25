c977c8c10b41dbdc546e33104bc1c8bced23527f
lianadanae <lianabette@gmail.com>
2020-01-25 11:57:33 -0500

    add new listing modal

--- client/src/pages/AddNewListing/index.js
+++ client/src/pages/AddNewListing/index.js
@@ -1,54 +1,127 @@
-// import React from 'react';
-// import { Button, ButtonToolbar, Form, Modal } from 'react-bootstrap';
+import React, { Component } from 'react';
+import API from "../../api";
+import Jumbotron from "../../components/Jumbotron";
+import { Col, Row, Container } from "../../components/Grid";
+import { Modal } from 'react-bootstrap';
+import { Input, TextArea, FormBtn } from "../../components/Form";
+import { Form, FormGroup } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+class AddNewListing extends Component {
+  state = {
+    listings: [],
+    address: "",
+    startDate: new Date(),
+    notes: ""
+  };
+
+  saveListing = () => {
+    API.saveListing()
+      .then(res =>
+        this.setState({ listings: res.data, address: "", startDate: new Date(), notes: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  //handleSelect 
 
 
-// render() {
-//     return (
-// function MyVerticallyCenteredModal(props) {
-//     return (
-//       <Modal
-//         {...props}
-//         size="lg"
-//         aria-labelledby="contained-modal-title-vcenter"
-//         centered
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title id="contained-modal-title-vcenter">
-//             Add a new listing
-//           </Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <h4>Centered Modal</h4>
-//           <p>
-//             Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-//             dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-//             consectetur ac, vestibulum at eros.
-//           </p>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button onClick={props.onHide}>Close</Button>
-//         </Modal.Footer>
-//       </Modal>
-//     );
-//   }
-  
-//   function AddNewListing() {
-//     const [modalShow, setModalShow] = React.useState(false);
-  
-//     return (
-//       <ButtonToolbar>
-//         <Button variant="primary" onClick={() => setModalShow(true)}>
-//           Launch vertically centered modal
-//         </Button>
-  
-//         <MyVerticallyCenteredModal
-//           show={modalShow}
-//           onHide={() => setModalShow(false)}
-//         />
-//       </ButtonToolbar>
-//     );
-//   }
-  
-//   render(<AddNewListing />);
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
 
-//   export default AddNewListing;
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.address && this.state.date) {
+      API.saveListing({
+        address: this.state.address,
+        date: this.state.date,
+        notes: this.state.notes
+      })
+        .then(res => {
+          // redirect back to listings
+          // when new listing successfully added
+          this.props.history.push('/listings');
+          //this.loadListings()
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Modal
+      show={true}
+      onHide={() => this.props.history.push('/listings')}
+      animation={false}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            New Listing
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>Add a new property listing</h1>
+            </Jumbotron>
+
+            <Form>
+              <FormGroup>
+              <Form.Label>Address</Form.Label>
+              <Input
+                value={this.state.address}
+                onChange={this.handleInputChange}
+                name="address"
+                placeholder="(required)"
+              />
+              <Form.Label>Date Listed</Form.Label>
+              <DatePicker
+                selected={this.state.date}
+                onSelect={this.handleSelect} //when day is clicked
+                onChange={this.handleChange} //only when value has changed
+                name="date"
+                placeholder="Date Listed (required)"
+              />
+              <Form.Label>Notes</Form.Label>
+              <TextArea
+                value={this.state.notes}
+                onChange={this.handleInputChange}
+                name="notes"
+                placeholder="(Optional)"
+              />
+              </FormGroup>
+            </Form>
+          </Col>
+          </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+        <FormBtn
+                disabled={!(this.state.date && this.state.address)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Listing
+              </FormBtn>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+};
+
+export default AddNewListing;
