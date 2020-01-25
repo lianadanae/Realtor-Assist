641d46a40febff1ddfb44a828700c2e03962aee3
lianadanae <lianabette@gmail.com>
2020-01-25 14:47:27 -0500

    added checkboxes

--- client/src/pages/Listing.js
+++ client/src/pages/Listing.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
-//import DeleteBtn from "../components/DeleteBtn";
+import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../api";
 import { Col, Row, Container } from "../components/Grid";
-import CardHolder from "../components/CardHolder";
-import { Card, Form } from "react-bootstrap";
+//import CardHolder from "../components/CardHolder";
+import { Card } from "react-bootstrap";
+import Moment from 'react-moment';
+import CheckBoxes from "../components/CheckBoxes";
 
 class Listings extends Component {
   state = {
     listings: [],
     address: "",
     startDate: new Date(),
-    notes: ""
+    notes: "",
+    contract: false,
+    mls: false,
+    showingTime: false,
+    compliance: false,
+    disclosures: false,
+    faceBook: false
   };
 
   componentDidMount() {
@@ -21,7 +29,7 @@ class Listings extends Component {
   loadListings = () => {
     API.getListings()
       .then(res =>
-        this.setState({ listings: res.data, address: "", startDate: new Date(), notes: "" })
+        this.setState({ listings: res.data, address: "", startDate: new Date(), notes: "", check: "" })
       )
       .catch(err => console.log(err));
   };
@@ -52,47 +60,22 @@ class Listings extends Component {
             <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
+  <DeleteBtn onClick={() => this.deleteListing(listing._id)} />
     <Card.Title>{listing.address}</Card.Title>
     <Card.Text>
+    <Moment format="MM/DD/YYYY">
     {listing.startDate}
+            </Moment>
+    </Card.Text>
+    <Card.Text>
     {listing.notes}
     </Card.Text>
   </Card.Body>
-  <Form>
-  <Form.Group controlId="formBasicCheckbox">
-  {['checkbox'].map(type => (
-    <div key={`custom-${type}`} className="mb-3">
-      <Form.Check 
-        custom
-        type={type}
-        id={`custom-${type}`}
-        label={`Check this custom ${type}`}
-      />
-      </div>
-      ))}
-      </Form.Group>
-      </Form>
+ 
+      <CheckBoxes></CheckBoxes>
 </Card>
             )}
         )}
-<CardHolder></CardHolder>
-
-            {/* {this.state.listings.length ? (
-              <List>
-                {this.state.listings.map(listing => (
-                  <ListItem key={listing._id}>
-                    <Link to={"/listings/" + listing._id}>
-                      <strong>
-                        {listing.address} by {listing.dateListed}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteListing(listing._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )} */}
           </Col>
         </Row>
       </Container>
