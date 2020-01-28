7bebc0a28bf2a248d28a1df61d56e307b98af31b
lianadanae <lianabette@gmail.com>
2020-01-28 16:29:22 -0500

    changed button colors

--- client/src/pages/Home/Splash.jsx
+++ client/src/pages/Home/Splash.jsx
@@ -1,46 +1,44 @@
 import React from "react";
 import { Button, Col, Row, Container, Jumbotron } from 'react-bootstrap';
+import "./style.css";
 
 export default function Splash(props) {
     return (
         <Container>
             <Row>
-                <Col >
-                    <Jumbotron fluid className='mt-4'>
-                        <img src="ralogo.png" alt="logo" className="text-center"></img>
-                        <h1 className='text-center'>Realtor Assist</h1>
-                        <p className='text-center'>Helping your Real Estate office run more efficiently</p>
-                    
+                <Col>
+                    <Jumbotron fluid className='mt-6'>
+                    <img src="ralogo.png" alt="logo" className="image"></img>
+                    </Jumbotron>
+                    <Jumbotron fluid className='mt-6'>
+                        <h1 className='text-center'>Helping your Real Estate office run more efficiently</h1>
                         <Row className="text-center">
                         <Col >
                             <Button 
                             type="submit" 
-                            className="btn btn-primary" 
+                            className="btn btn-success" 
+                            style={{ fontSize: '1rem' }}
                             onClick={() => props.history.push('/signup')}
                             >Signup</Button>
                         </Col>
                         <Col >
                             <Button 
                             type="submit" 
-                            className="btn btn-primary"
+                            className="btn btn-success"
+                            style={{ fontSize: '1rem' }}
                             onClick={() => props.history.push('/login')}
                             >Login</Button>
                         </Col>
                     </Row>
                     </Jumbotron>
                     
-                    <Jumbotron fluid className='mt-4'>
-                        <h1 className='text-center'>Helping real estate assistants & coordinators create organizational systems for the real estate office</h1>
+                    {/* <Jumbotron fluid className='mt-4'>
+                        <h1 className='text-center'>Helping real estate assistants & coordinators create organizational systems for their office</h1>
                         <Row className="text-center">
                         <Col >
-                        {/* <Button 
-                            type="submit" 
-                            className="btn btn-primary" 
-                            onClick={() => props.history.push('/signup')}
-                            >Learn More</Button> */}
                             </Col>
                             </Row>
-                    </Jumbotron>
+                    </Jumbotron> */}
                 </Col>
             </Row>
         </Container>
