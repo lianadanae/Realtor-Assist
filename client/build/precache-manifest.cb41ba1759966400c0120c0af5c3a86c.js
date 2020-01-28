aa9e0009983af2e178e7636a6d10133f0ecf2a84
lianadanae <lianabette@gmail.com>
2020-01-27 21:34:05 -0500

    npm run build

---
+++ client/build/precache-manifest.cb41ba1759966400c0120c0af5c3a86c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4d2481710cf99e41c2685c3c6b232a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f35cca261f7a8616ec16",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "eb6544d29d1c9fc561d4",
+    "url": "/static/css/main.d91650ce.chunk.css"
+  },
+  {
+    "revision": "f35cca261f7a8616ec16",
+    "url": "/static/js/2.620c99e8.chunk.js"
+  },
+  {
+    "revision": "0495fb0a28f8842ed28efd755b60da02",
+    "url": "/static/js/2.620c99e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb6544d29d1c9fc561d4",
+    "url": "/static/js/main.7a3730ea.chunk.js"
+  },
+  {
+    "revision": "1d7ee574847c68b76183",
+    "url": "/static/js/runtime-main.b5adf652.js"
+  }
+]);
