d7e77f86614cbc598f7e0874135221a9fbd3835b
lianadanae <lianabette@gmail.com>
2021-07-01 17:32:25 -0600

    password change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const passport = require('passport');
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
+  
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,6 +23,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+
 
 // Start the API server
 app.listen(PORT, function() {
